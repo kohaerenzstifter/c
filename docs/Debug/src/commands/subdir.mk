fecc7bbb4c5856c54b4607e30ef4f01239422b94
Martin Knappe <kohaerenzstifter@posteo.de>
2014-10-05 13:53:17 +0200

    just a build configuration change

--- docs/Debug/src/commands/subdir.mk
+++ docs/Debug/src/commands/subdir.mk
@@ -35,7 +35,7 @@ C_DEPS += \
 src/commands/%.o: ../src/commands/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I"/home/sancho/Desktop/k/kohaerenzstiftung" -I/usr/include/glib-2.0/ -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc -I"/home/sancho/git/c/kohaerenzstiftung" -I/usr/include/glib-2.0/ -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
